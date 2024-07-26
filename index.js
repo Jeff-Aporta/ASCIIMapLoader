6b5f2a4acfa395156f1213ffed24dae29406fda2
Jeffrey Agudelo <jeffaporta@gmail.com>
2024-07-25 20:04:23 -0500

    ASCII Map Loader

---
+++ index.js
@@ -0,0 +1,342 @@
+/*
+    EJEMPLO
+
+          ➤📁 public
+            ├──📁 css
+            │   ├──🏠 https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js
+            ╧   └──📄 code-prettify.css
+
+          ➤🌐 https://cdn.jsdelivr.net/gh/Jeff-Aporta/react-cdn-design-system@latest
+            ├──📄 index
+            │
+            ├──📁 dependencias
+            │   └──🎨 animate.min.css
+            │
+            🤖 REACT (JSX)
+            │
+            ├──📄 main
+            │
+            ├──📁 componente
+            │   ├──📄 selectpicker
+            │   ├──📄 switchTheme
+            │   ├──📄 tiempo
+            │   │
+            │   ├──📁 ventanas
+            │   │   │
+            │   │   ├──📁 flotante-tipo-windows
+            │   │   │   ├──📄 1-main
+            │   │   │   ├──📄 2-modelos
+            │   │   │   ├──📄 2-globales
+            │   │   │   ├──📄 2-css-root-control
+            │   │   │   ├──📄 index.css
+            │   │   │   │
+            │   │   │   ├──📁 componente
+            │   │   │   │   ├──📄 3-crear
+            │   │   │   │   ├──📄 3-marco
+            │   │   │   │   ├──📄 3-control
+            │   │   ╧   ╧   ╧
+            │   ├──🗀 reproductor
+            │   │   ├──📁 video
+            ╧   ╧   ╧   └──📄 theme1
+
+          ➤📁 paginas
+            │
+            🤖 REACT (JSX)
+            │
+            ├──📄 configuracion
+            ├──📄 librerias
+            ├──📄 perfil
+            ├──📄 proyectos
+            │
+            ├──📁 Librerias
+            ╧   └──📄 JFTP
+
+          🤖 REACT (JSX)
+
+          ➤📄 index-app
+          ➤📄 fin
+ */
+
+function runASCIIMapLoad_inLine(asciiMaploader) {
+  recorrerRecursivamenteTodosLosNodos(ASCIIMapLoader2JSON(asciiMaploader));
+
+  function recorrerRecursivamenteTodosLosNodos(node, rutaAcumulada) {
+    node.list.forEach((e) => {
+      if (e.folder) {
+        recorrerRecursivamenteTodosLosNodos(
+          e,
+          [rutaAcumulada, e.folder].filter((e) => e).join("/")
+        );
+      } else if (e.archivo) {
+        writeScript({
+          archivo: [rutaAcumulada, e.archivo].filter((e) => e).join("/"),
+          type: e.type,
+        });
+      }
+    });
+  }
+
+  function writeScript({ archivo, type }) {
+    let ext;
+    intuirExtension();
+
+    let stringLoad =
+      ext == ".css"
+        ? `<link rel="stylesheet" href="${archivo}">`
+        : `<script type="${type}" src="${archivo}${ext ?? ""}"><\/script>`;
+
+    console.log(stringLoad);
+
+    document.write(stringLoad);
+
+    function intuirExtension() {
+      const isJSX = archivo.endsWith(".jsx");
+      const isJS = archivo.endsWith(".js");
+      const isCSS = archivo.endsWith(".css");
+      if (isJSX || isJS) {
+        protocoloDeExtensionQuemada();
+      } else if (!isCSS) {
+        protocoloDeExtensionTipada();
+      } else {
+        ext = ".css";
+      }
+
+      function protocoloDeExtensionTipada() {
+        switch (type) {
+          case "text/javascript":
+            ext = ".js";
+            break;
+          case "text/babel":
+            ext = ".jsx";
+            break;
+        }
+      }
+
+      function protocoloDeExtensionQuemada() {
+        if (isJSX) {
+          type = "text/babel";
+        } else if (isJS) {
+          type = "text/javascript";
+        }
+      }
+    }
+  }
+
+  //---------------- conversor de ASCIIMapLoader a JSON
+
+  function ASCIIMapLoader2JSON(stringASCIIMapLoader) {
+    const ignorar = ["➤", "──"];
+    const indicadorDeNivel = ["├", "└", "│", "╧"];
+    const decoradores = {
+      enRaiz: "🏠",
+      CSS: "🎨",
+      JSXCSS: "🖼️",
+      ramas: ["🌐", "📁", "🗀"],
+      hojas: ["🔗", "📄", "🎨", "🖼️", "🏠"],
+      comandos: ["🤖"],
+    };
+
+    let renglones = stringASCIIMapLoader
+      .split("\n")
+      .map((e) => ignorar.reduce((acc, c) => acc.replace(c, ""), e))
+      .map((e) =>
+        e.includes(decoradores.enRaiz)
+          ? indicadorDeNivel.reduce((acc, c) => acc.replace(c, ""), e)
+          : e
+      )
+      .map((e) => e.trim())
+      .filter(
+        (e) =>
+          e != "" &&
+          !e
+            .split("")
+            .every((e) => indicadorDeNivel.some((c) => c == e) || e == " ")
+      );
+
+    {
+      let renglonesJSXCSS = [];
+
+      renglones.forEach((e) => {
+        if (e.includes(decoradores.JSXCSS)) {
+          renglonesJSXCSS.push(e.replace(decoradores.JSXCSS, "📄"));
+          renglonesJSXCSS.push(e.replace(decoradores.JSXCSS, "🎨"));
+        } else {
+          renglonesJSXCSS.push(e);
+        }
+      });
+
+      renglones = renglonesJSXCSS;
+    }
+
+    let enRaiz = renglones.filter((e) => e.includes(decoradores.enRaiz));
+    renglones = renglones.filter((e) => !e.includes(decoradores.enRaiz));
+
+    renglones = [...enRaiz, ...renglones];
+
+    let retorno = refinarArbol(generarArbol());
+
+    return retorno;
+
+    function refinarArbol(arbol) {
+      refinacionGeneral();
+      refinarArchivo();
+      recorrerTodosLosNodos();
+      refinarCarpeta();
+
+      return arbol;
+
+      function refinacionGeneral() {
+        delete arbol.padre;
+        delete arbol.nivel;
+      }
+
+      function refinarArchivo() {
+        if (arbol.archivo) {
+          arbol.archivo = limpiarNombre(arbol.archivo);
+        }
+      }
+
+      function refinarCarpeta() {
+        if (arbol.folder != undefined) {
+          arbol.folder = limpiarNombre(arbol.folder);
+          delete arbol.type;
+        }
+      }
+
+      function recorrerTodosLosNodos() {
+        if (arbol.list) {
+          removerCarpetasInactivas();
+          recorrerRecursivamenteTodosLosNodos();
+        }
+
+        function recorrerRecursivamenteTodosLosNodos() {
+          arbol.list = arbol.list.map((e) => refinarArbol(e));
+        }
+
+        function removerCarpetasInactivas() {
+          arbol.list = arbol.list.filter(
+            (e) => e.archivo || (e.folder && !e.folder.includes("🗀"))
+          );
+        }
+      }
+
+      function limpiarNombre(nombre) {
+        nombre = [
+          indicadorDeNivel,
+          decoradores.hojas,
+          decoradores.ramas,
+        ].reduce(
+          (resultado, coleccion) =>
+            (resultado = coleccion.reduce(
+              (acc, c) => (acc = acc.replaceAll(c, "")),
+              resultado
+            )),
+          nombre
+        );
+        nombre = nombre.trim();
+        return nombre;
+      }
+    }
+
+    function generarArbol({
+      padre = {
+        padre: null,
+        folder: "",
+        list: [],
+        type: "text/javascript",
+        nivel: -1,
+      },
+      node,
+    } = {}) {
+      let renglon = node ? "" : renglones.shift();
+
+      if (renglon == undefined) {
+        return padre;
+      }
+
+      let determinarNivel = renglon
+        .split("")
+        .filter((e) => indicadorDeNivel.some((c) => c == e)).length;
+
+      if (node) {
+        if (padre.nivel < node.nivel) {
+          padre.list.push(node);
+          node.padre = node.padre.padre;
+          generarArbol({ padre: node });
+        } else {
+          generarArbol({ padre: padre.padre, node });
+        }
+      } else {
+        let type = padre.type;
+
+        if (decoradores.comandos.some((e) => renglon.includes(e))) {
+          ejecutarComando();
+          generarArbol({ padre });
+        } else {
+          const esRama = decoradores.ramas.some((e) => renglon.includes(e));
+          const esHoja = decoradores.hojas.some((e) => renglon.includes(e));
+
+          if (esRama) {
+            const newRoot = {
+              padre,
+              folder: renglon,
+              type: padre.type,
+              list: [],
+              nivel: determinarNivel,
+            };
+
+            if (determinarNivel > padre.nivel) {
+              padre.list.push(newRoot);
+              generarArbol({
+                padre: newRoot,
+              });
+            } else {
+              generarArbol({
+                padre: padre.padre,
+                node: newRoot,
+              });
+            }
+          } else if (esHoja) {
+            if (renglon.includes(decoradores.CSS)) {
+              if (!renglon.endsWith(".css")) {
+                renglon += ".css";
+              }
+            }
+            const newRoot = {
+              padre,
+              archivo: renglon,
+              type: padre.type,
+              nivel: determinarNivel,
+            };
+
+            if (padre.folder != undefined && determinarNivel > padre.nivel) {
+              padre.list.push(newRoot);
+              generarArbol({
+                padre: newRoot,
+              });
+            } else {
+              generarArbol({
+                padre: padre.padre,
+                node: newRoot,
+              });
+            }
+          }
+        }
+        return padre;
+      }
+
+      function ejecutarComando() {
+        let comandos = Object.entries({
+          "JAVASCRIPT (JS)": () => {
+            padre.type = "text/javascript";
+          },
+          "REACT (JSX)": () => {
+            padre.type = "text/babel";
+          },
+        })
+          .filter(([k]) => renglon.includes(k))
+          .forEach(([k, v]) => v());
+      }
+    }
+  }
+}
---
+++ mapeos-personales.js
@@ -0,0 +1,78 @@
+/*
+https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.all.min.js
+https://cdn.jsdelivr.net/npm/@sweetalert2/theme-dark/dark.css
+*/
+
+const ASCII_MAPS = {
+  CODEPRETTIFY: `
+        🔗 https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js
+    `,
+  ICONOS: `
+        🔗 https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css
+        🔗 https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css
+    `,
+  REACT_MUI: `
+        ➤🌐 https://unpkg.com
+        │
+        ├──🔗 react@latest/umd/react.production.min.js
+        ├──🔗 react-dom@latest/umd/react-dom.production.min.js
+        ├──🔗 @babel/standalone/babel.min.js
+        └──🔗 @mui/material@latest/umd/material-ui.production.min.js
+    `,
+  REACT_DESIGN_SYSTEM: `
+            ➤🌐 https://cdn.jsdelivr.net/gh/Jeff-Aporta/react-cdn-design-system@main
+            │
+            ├──📄 JS2CSS
+            ├──🎨 abreviaturas
+            │
+            ├──📁 dependencias
+            │   └──🎨 animate.min
+            │
+            🤖 REACT (JSX)
+            │
+            ├──📄 main
+            │
+            ├──📁 componente
+            │   │
+            │   ├──📁 MUICustom
+            │   │   ├──📄 prettoSlider
+            │   │   ├──📄 switchTheme
+            │   │   └──📄 toolTipTheme
+            │   │
+            │   ├──📁 selects
+            │   │   ├──🖼️ selector-tiempo
+            │   │   └──🖼️ selectpicker
+            │   │
+            │   ├──📁 ventanas
+            │   │   │
+            │   │   ├──📁 flotante-tipo-windows
+            │   │   │   ├──📄 1-main
+            │   │   │   ├──📄 2-modelos
+            │   │   │   ├──📄 2-globales
+            │   │   │   ├──📄 2-css-root-control
+            │   │   │   ├──🎨 index
+            │   │   │   │
+            │   │   │   ├──📁 componente
+            │   │   │   │   ├──📄 3-crear
+            │   │   │   │   ├──📄 3-marco
+            │   │   ╧   ╧   └──📄 3-control
+            │   │
+            │   ├──📁 reproductor
+            │   │   ├──📁 video
+            ╧   ╧   ╧   └──📄 theme1
+    `,
+  PORTAFOLIO: `
+            ➤🌐 https://cdn.jsdelivr.net/gh/Jeff-Aporta/portafolio@u2024.07.25.7
+            │
+            🤖 REACT (JSX)
+            │
+            ├──📁 src
+            │   │
+            │   ├──📁 css
+            │   │   └──🎨 code-prettify
+            │   │
+            │   ├──📁 jsx
+            │   │   ├──🖼️ componentes
+            ╧   ╧   └──📄 pagina
+    `,
+};
