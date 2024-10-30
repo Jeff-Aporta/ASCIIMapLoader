5138aa96cbb3d645e61b05a7ba37f88683454143
Jeffrey Agudelo <jeffaporta@gmail.com>
2024-10-30 16:44:23 -0500

    Revert "[fix] inferencia de .mjs cuando es módulo"
    This reverts commit 3c54b6b43a5d257d147e768160efc68c44d0f110.

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ function runASCIIMapLoader_inLine(asciiMaploader) {
       }
     })();
 
+    console.log(stringLoad);
+
     document.write(stringLoad);
 
     function intuirExtension() {
@@ -278,11 +280,15 @@ function runASCIIMapLoader_inLine(asciiMaploader) {
             };
 
             if (padre.folder != undefined && determinarNivel > padre.nivel) {
+              console.log("bif 1");
+              console.log(newRoot);
               padre.list.push(newRoot);
               generarArbol({
                 padre: newRoot,
               });
             } else {
+              console.log("bif 2");
+              console.log(newRoot);
               generarArbol({
                 padre: padre.padre,
                 node: newRoot,
